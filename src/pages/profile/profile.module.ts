fc7d3b3ff0db4ab6339ac56900802a0c6a2f521d
akash-uniterrene <akash@uniterrene.com>
2018-10-31 16:18:11 +0530

    ranit changes

--- src/pages/profile/profile.module.ts
+++ src/pages/profile/profile.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ProfilePage } from './profile';
-import { WhatsOnMindComponent } from '../../components/whats-on-mind/whats-on-mind';
+
 @NgModule({
   declarations: [
-    ProfilePage,
-    WhatsOnMindComponent
+    ProfilePage,   
   ],
   imports: [
     IonicPageModule.forChild(ProfilePage),
